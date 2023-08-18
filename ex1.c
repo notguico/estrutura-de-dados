b0c28ebf00a0a0be5d52faea8bc6f82f8623fc7b
Guilherme Werka <141884850+notguico@users.noreply.github.com>
2023-08-17 21:42:31 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+	
+	int vetor[10];
+	int n, k, i, j;
+	
+	printf("Informe o tamanho do vetor (maximo 10):\n");
+	scanf("%d", &n);
+	printf("Insira o valor de k:\n");
+	scanf("%d", &k);
+	
+	printf("Insira os valores do vetor: \n");
+	for(i=0;i<n;i++) {
+		scanf("%d", &vetor[i]);
+	}
+	
+	for(i=0;i<n;i++) {
+		for(j=0;j<n;j++){
+			if(i != j){
+				if ((vetor[i] + vetor[j]) == k) {
+					printf("Os valores %d + %d somam %d.\n", vetor[i], vetor[j], k);
+					return 1;
+				}
+			}
+		}
+
+	}
+	printf("Nenhum par soma a k.\n");
+	
+	
+	return 0;
+}
